682d6072725156047c7eb8fc039035f20201e22f
MatteyGG <matgaranin@gmail.com>
2026-04-08 01:46:45 +0300

    fix(Config): allow Directus asset host in next/image remotePatterns.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "selfcms.stasis-wp.ru",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
